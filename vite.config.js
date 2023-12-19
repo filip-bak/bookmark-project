be0304a0b3192bef80ab452179839a2abef50989
Filip Bączkowski <filo.b@wp.pl>
2023-12-19 23:21:10 +0100

    chore: add css config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@icons": path.resolve(__dirname, "./src/assets/images/svg"),
+    },
+  },
+  css: {
+    modules: {
+      generateScopedName: `[folder]_[local]__[hash:base64:5]`,
+    },
+  },
 });
